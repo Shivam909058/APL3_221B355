5cf077599826960fd18470568f0a0a75d48ca9d2
221B355 <221B355@juet.co.in>
2025-04-16 15:24:05 +0530

    adapter pattern

---
+++ AP_LAB(adapter)/IndianCrow.java
@@ -0,0 +1,15 @@
+public class IndianCrow implements Crow{
+    public void cry(){
+        System.out.println("cawing");
+    }
+
+    public void fly(){
+        System.out.println("flying ");
+    }
+
+    public void eat(){
+        System.out.println("eating pearls");
+    }
+
+    
+}
---
+++ AP_LAB(adapter)/IndianSwan.java
@@ -0,0 +1,15 @@
+public class IndianSwan implements Swan{
+    public void swim(){
+        System.out.println("swim");
+    }
+
+    public void sing(){
+        System.out.println("sing");
+    }
+
+    public void eat(){
+        System.out.println("eattt");
+    }
+
+    
+}
